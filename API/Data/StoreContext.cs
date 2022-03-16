7483b787565c954b13fa393d5fe8917a6157a404
kevin-modern <webadmin@modernbeauty.com>
2022-03-16 16:29:51 -0600

    1. Added Entities > Product.cs 2. Added Data > StoreContext.cs 3. Updated appsettings.Development to add ConnectionString(DB Settings) 4. Updated Startup.cs to add service for using DbContext and SQLITE

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        // 아래는 option을 가진 constructor임
+        // 기본꼴에서 DbContextOptions의 타입을 Class 이름인 StoreContext로 지정 해주기. 이렇게 해야지 나중에 여러개의 DbContext가 생겨도 구별이 가능 함.
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+
+        // 아래와 같이 DbSet type으로 변수 지정 하기
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
