8b7dc6108ec4ccc933da0b94665137bf2a389cf4
kevin-modern <webadmin@modernbeauty.com>
2022-03-17 16:28:55 -0600

    Repository 생성을 위한 IProductRepository.ca 인터페이스 생성

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync(); // 그냥 Product List를 리턴하는 method이기 때문에 쓰기나, 수정과 같은 기능이 필요없음. 따라서 IReadOnlyList라는 specific한 type으로 지정 해 줌.
+    }
+}
