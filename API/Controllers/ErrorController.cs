bdbc60a5c938a053878f25bdd52183cc26fafc4d
kevin-modern <webadmin@modernbeauty.com>
2022-03-22 11:06:50 -0600

    Added IgnoreApi option to Error Controller, DOWNGRADE Swashbuckle.AspNetCore version from 6.2.3 to 6.2.2 due to internal server error

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -3,11 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+
     [Route("errors/{statusCode}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class ErrorController : BaseApiController
+
     {
 
         public IActionResult Error(int statusCode)
+
         {
 
             return new ObjectResult(new ApiResponse(statusCode));
