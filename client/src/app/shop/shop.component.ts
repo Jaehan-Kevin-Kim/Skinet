8cf0a3308cf537a1a3e7e6ff3ecee7fd700553fa
kevin-modern <webadmin@modernbeauty.com>
2022-04-06 16:10:17 -0600

    Fixed bugs when choosing filter options from page 2 or 3

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -81,15 +81,16 @@ export class ShopComponent implements OnInit {
   }
 
   onBrandSelected(brandId: number) {
-    console.log('brandId: ', brandId);
-
+    // console.log('brandId: ', brandId);
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
-    console.log('typeId: ', typeId);
+    // console.log('typeId: ', typeId);
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -99,12 +100,17 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    console.log('onpagechanged event: ', event);
+
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
