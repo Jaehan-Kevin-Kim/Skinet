53d52e904c37e2b5b7f39ec44c764c4c676ccb05
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-04-10 19:54:29 -0600

    Displaying the basket item count in the nav bar. (shopping cart icon에 현재 item 갯수 출력, shipping cart icon 클릭시 출력되는 page에 현재 basket에 있는 item들 목록 출력)

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { BasketService } from '../../basket/basket.service';
+import { Observable } from 'rxjs';
+import { IBasket } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,10 +12,12 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 export class NavBarComponent implements OnInit {
 
   faCartShopping = faCartShopping
+  basket$: Observable<IBasket>
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
+    this.basket$ = this.basketService.basket$
   }
 
 }
