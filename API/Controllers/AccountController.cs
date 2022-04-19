66f880b84ed001a6b2ddb1b27625f773933f8272
Jaehan-Kevin-Kim <jhkevin21@gmail.com>
2022-04-18 21:35:02 -0600

    Added additional account method in AccountController.cs - GetCurrentUser() - CheckEmailExistsAsync() - GetUserAddress()

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
 using Core.Entities.Identity;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +21,50 @@ namespace API.Controllers
             _signInManager = signInManager;
             _userManager = userManager;
         }
+
+
+
+        // 아래는 token 만들때 생성했던 claim 을 이용해서 user를 찾는 API.
+        // 아래와 같이 Authorize가 추가 된 경우는 headers에 authorization 항목에 value(값)을 추가 해 줘야 함.
+        // [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+
+            var user = await _userManager.FindByEmailAsync(email);
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user),
+                DisplayName = user.DisplayName
+            };
+        }
+
+        // 아래는 user 등록을 하는 경우 이미 존재하는 email이 있는지 여부를 확인 하는 API. 여기서 직접적으로 처리를 한다기 보다, email이 존재 하는 경우 true를 넘겨줘서 이걸 client 측에서 처리 할 예정.
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
+        {
+            return await _userManager.FindByEmailAsync(email) != null;
+        }
+
+
+        // 아래는 UserAddress를 찾는 API.
+        [Authorize]
+        [HttpGet("address")]
+        public async Task<ActionResult<Address>> GetUserAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+
+            var user = await _userManager.FindByEmailAsync(email);
+
+            return user.Address;
+
+
+        }
+
+
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
